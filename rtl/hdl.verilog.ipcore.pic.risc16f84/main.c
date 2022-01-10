24ef3103bdafca8a76af8422f588d1ed0fb2f2ed
pawlex <pawlex@gmail.com>
2022-01-10 01:28:02 -0800

    more pic makefile automation for flashing

--- rtl/hdl.verilog.ipcore.pic.risc16f84/main.c
+++ rtl/hdl.verilog.ipcore.pic.risc16f84/main.c
@@ -1,3 +1,13 @@
+/* Paul Komurka
+ * test file for the soft-core pic16f84 (clk2x)
+ * with ports A/B removed and replaced with a 
+ * GPIO BUS.
+ *
+ * IO BUS:
+ * ADDRESS [15:0] = { PORTB,PORTA }
+ * DATA    [07:0] =   EEDATA
+*/
+
 #include <pic16regs.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -11,10 +21,10 @@ void halt(void)
     }
 }
 
-#define STRIDE 0x1
-#define MAXVAL 0xFF
-
-void nopsleep(uint16_t target)
+// Until we get a proper timer, this is the best we can do.
+// TODO: Profile how long the call/return/nop takes so this
+// can be somewhat calibrated:  similar to usleep(100).
+void nop_sleep(uint16_t target)
 {
 	for(uint16_t i=target; i>0; i--)
 	{
@@ -27,57 +37,31 @@ void io_data_write(uint8_t val, uint16_t sleep)
     EEDATA = val;
     if(sleep)
     {
-        nopsleep(sleep);
+        nop_sleep(sleep);
     }
 }
 
 void led_test(void)
 
 {
-    //io_data_write(0xff, 0xF000);
-    //io_data_write(0x00, 0xF000);
     io_data_write(0x55, 0xF000);
     io_data_write(0xAA, 0xF000);
 }
 
 void led_scan(uint16_t sleep)
 {
-   // uint8_t i=1;
-   // for(uint8_t j=64; j>0; j--)
-   // {
-   //     i=i<<1;
-   //     io_data_write(i, sleep);
-   // }
     uint8_t shift = 1;
     do
     {
        shift = shift << 1;
-       io_data_write(shift, 0x1000);
+       io_data_write(shift, sleep);
     } while (shift < 0x80);
     do
     {
        shift = shift >> 1;
-       io_data_write(shift, 0x1000);
+       io_data_write(shift, sleep);
     } while (shift > 0x1);
-}
-
-void led_scan_noshift(uint16_t sleep)
-{
-    io_data_write(0x01, sleep);
-    io_data_write(0x02, sleep);
-    io_data_write(0x04, sleep);
-    io_data_write(0x08, sleep);
-    io_data_write(0x10, sleep);
-    io_data_write(0x20, sleep);
-    io_data_write(0x40, sleep);
-    io_data_write(0x80, sleep);
-    io_data_write(0x40, sleep);
-    io_data_write(0x20, sleep);
-    io_data_write(0x10, sleep);
-    io_data_write(0x08, sleep);
-    io_data_write(0x04, sleep);
-    io_data_write(0x02, sleep);
-    io_data_write(0x01, sleep);
+    nop_sleep(0x4000);
 }
 
 void main(void)
@@ -85,22 +69,12 @@ void main(void)
 	while(1)
 	{
 	    //led_test();
-        led_scan(0x1000);
-        //led_scan_noshift(0x800);
+        led_scan(0x800);
 	}
 
     halt();
 }
 
-/* Paul Komurka
- * test file for the soft-core pic16f84 (clk2x)
- * with ports A/B removed and replaced with a 
- * GPIO BUS.
- *
- * IO BUS:
- * ADDRESS [15:0] = { PORTB,PORTA }
- * DATA    [7:0 ] =   EEDATA
- */
 
 
 // LOAD GPIO RAM (RAM BACKED PORT)
